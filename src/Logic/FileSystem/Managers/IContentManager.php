3227ea00ab3efa97977c9b08d2fa945fdf68081f
steamvis <steamvis@yandex.ru>
2020-03-25 17:13:35 +0300

    Добавил контент менеджера для работы с разными системами хранения контента

---
+++ src/Logic/FileSystem/Managers/IContentManager.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Logic\FileSystem\Managers;
+
+/**
+ * Interface IContentManager
+ *
+ * Интерфейс для управления контентом разных систем
+ * fileSystem, databaseSystem...
+ *
+ * @package Logic\FileSystem\Managers
+ */
+interface IContentManager
+{
+    public function saveContent(string $content, string $savePath): void;
+
+    public function getContent(string $path): string;
+}
